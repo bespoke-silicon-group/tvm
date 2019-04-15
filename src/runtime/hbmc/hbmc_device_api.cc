606c01e9a47e784382d3f9a47f3770236508512a
Chien-Yu Lin <cyulin@cs.washington.edu>
2019-04-15 07:17:46 +0000

    add device api code

--- src/runtime/hbmc/hbmc_device_api.cc
+++ src/runtime/hbmc/hbmc_device_api.cc
@@ -19,6 +19,7 @@ class HBMCDeviceAPI final : public DeviceAPI {
     LOG(INFO) << "Call HBMCDeviceAPI::SetDevice()";
 
     hb_mc_init_host((uint8_t*)&(ctx.device_id));
+    LOG(INFO) << "ctx.device_id: " << ctx.device_id;
 
     tile_t tiles[1];
     tiles[0].x = 0;
@@ -48,15 +49,18 @@ class HBMCDeviceAPI final : public DeviceAPI {
     eva_id_t eva_id = 0; // set eva_id to zero to make malloc func work
 
     eva_t ptr = hb_mc_device_malloc(eva_id, nbytes);
+    printf("malloc addr: 0x%x\n", ptr);
 
-    return static_cast<void *>(&ptr);
+    return (void*) ptr;
   }
 
   void FreeDataSpace(TVMContext ctx, void* ptr) final {
     std::cout << "Call HBMC FreeDataSpace()\n";
+    printf("free addr: 0x%x\n", reinterpret_cast<uint32_t*>(ptr));
 
     eva_id_t eva_id = 0; // set eva_id to zero to make malloc func work
-    hb_mc_device_free(eva_id, static_cast<uint32_t>(reinterpret_cast<std::uintptr_t>(ptr)));
+    hb_mc_device_free(eva_id, static_cast<uint32_t>(
+                              reinterpret_cast<std::uintptr_t>(ptr)));
   }
 
   void CopyDataFromTo(const void* from,
@@ -70,12 +74,42 @@ class HBMCDeviceAPI final : public DeviceAPI {
                       TVMStreamHandle stream) final {
     std::cout << "Call HBMC CopyDataFromTo()\n";
 
-    std::cout << "ctx_from: " << ctx_from.device_id << std::endl;
-    std::cout << "ctx_to: " << ctx_to.device_id << std::endl;
+    std::cout << "size: " << size << std::endl;
+    std::cout << "ctx_from.device_id: " << ctx_from.device_id 
+              << ", ctx_from.device_type: " << ctx_from.device_id << std::endl;
+    std::cout << "ctx_to.device_id: " << ctx_to.device_id 
+              << ", ctx_to.device_type: " << ctx_to.device_id << std::endl;
 
-    //if (hb_mc_device_memcpy(hbmc_device_id, eva_id, dst, src, size_buffer * sizeof(uint32_t), hb_mc_memcpy_to_device) != HB_MC_SUCCESS) {
-      //printf("Could not copy buffer A to device.\n");
-    //}
+    eva_id_t eva_id = 0; // set eva_id to zero to make malloc func work
+    printf("memcpy addr: 0x%x\n", reinterpret_cast<uint32_t*>(to));
+
+    if (hb_mc_device_memcpy(ctx_to.device_id, eva_id, to, from, 
+                            size, hb_mc_memcpy_to_device) != HB_MC_SUCCESS) {
+      printf("Could not copy buffer A to device.\n");
+    }
+    
+    /***** test whether the hb_mc_device_memcpy() is correct *****/
+    /*
+    hb_mc_response_packet_t* buf = new hb_mc_response_packet_t[size/sizeof(uint32_t)];
+    void* dst = (void *) buf;
+    if (hb_mc_device_memcpy(ctx_to.device_id, eva_id, dst, to, size, 
+                            hb_mc_memcpy_to_host)) {
+      LOG(FATAL) << "Unable to memory copy from device to host";
+    }
+
+    uint32_t *arr = static_cast<uint32_t*>((void*)from);
+    printf("from[0:8]: ");
+    for (int i = 0; i < 8; i++)
+      printf("%u ", arr[i]); 
+    printf("\n");
+    
+    printf("buf[0:8]: ");
+    for (int i = 0; i < 8; i++) {
+      printf("%u ", hb_mc_response_packet_get_data(&buf[i]));
+    }
+    printf("\n");
+    */
+
   }
 
   void StreamSync(TVMContext ctx, TVMStreamHandle stream) final {
