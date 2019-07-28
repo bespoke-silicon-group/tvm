be4a5033d802b668bd4604e205c6f775acc6d9d8
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-07-28 02:10:56 +0000

    Added max_pool2d cuda_lite test

---
+++ tutorials/cuda_lite/max_pool2d_cuda_lite.py
@@ -0,0 +1,84 @@
+"""
+Get Started with TVM
+====================
+**Author**: `Tianqi Chen <https://tqchen.github.io>`_
+
+This is an introduction tutorial to TVM.
+TVM is a domain specific language for efficient kernel construction.
+
+In this tutorial, we will demonstrate the basic workflow in TVM.
+"""
+from __future__ import absolute_import, print_function
+
+import tvm
+import numpy as np
+
+# Global declarations of environment.
+dtype = 'int32'
+
+M = 16
+N = 4
+#sub_block = M / N
+sub_block = 4
+n = tvm.var("n")
+
+k = tvm.reduce_axis((0, sub_block), 'k')
+j = tvm.reduce_axis((0, sub_block), 'j')
+A = tvm.placeholder((M, M), name="A", dtype=dtype)
+B = tvm.compute(
+        (N, N),
+        lambda x, y: tvm.max(A[(x * sub_block + k), (y * sub_block + j)], axis=[k, j]),
+        name='B')
+
+
+
+scale = 4
+num_thread = 1
+block_factor = scale*num_thread
+
+s = tvm.create_schedule(B.op)
+
+block_x = tvm.thread_axis("blockIdx.x")
+thread_x = tvm.thread_axis((0, num_thread), "threadIdx.x")
+block_y = tvm.thread_axis("blockIdx.y")
+thread_y = tvm.thread_axis((0, num_thread), "threadIdx.y")
+
+by, yi = s[B].split(B.op.axis[0], factor=block_factor)
+bx, xi = s[B].split(B.op.axis[1], factor=block_factor)
+s[B].bind(by, block_y)
+s[B].bind(bx, block_x)
+s[B].reorder(by, bx, yi, xi)
+
+ty, yi = s[B].split(yi, nparts=num_thread)
+tx, xi = s[B].split(xi, nparts=num_thread)
+s[B].bind(ty, thread_y)
+s[B].bind(tx, thread_x)
+s[B].reorder(ty, tx, yi, xi)
+
+tgt_host="llvm"
+# Use cuda_lite or hbmc to generate code for hammerblade
+tgt="cuda_lite"
+
+func = tvm.build(s, [A, B], tgt, target_host=tgt_host, name="cuda_lite_max_pool2d")
+
+if tgt == "cuda_lite" or tgt.startswith('opencl'):
+    dev_module = func.imported_modules[0]
+    print("-----CUDA Lite code-----")
+    print(dev_module.get_source())
+#exit()
+
+ctx = tvm.context(tgt, 0)
+
+a_np = np.random.randint(10, size=(M ,M)).astype(dtype)
+b_np = np.zeros((N, N), dtype=B.dtype)
+
+a = tvm.nd.array(a_np, ctx)
+b = tvm.nd.array(b_np, ctx)
+func(a, b)
+print (a)
+print (b)
+
+#ans = np.dot(a.asnumpy(), b.asnumpy())
+#err = tvm.testing.assert_allclose(c.asnumpy(), ans, rtol=1e-5)
+#if not err:
+#    print("The resutls of matrix multiplication are correct.")
