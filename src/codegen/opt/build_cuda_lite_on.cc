80f36db2a66a13dffdb8438a1ad8af2df22f1163
Chien-Yu Lin <cyulin@cs.washington.edu>
2019-08-02 16:28:35 -0700

    crt.o

--- src/codegen/opt/build_cuda_lite_on.cc
+++ src/codegen/opt/build_cuda_lite_on.cc
@@ -59,12 +59,14 @@ runtime::Module BuildCUDALite(Array<LoweredFunc> funcs) {
   std::string main_o = std::string(tvm_p) + "/tutorials/cuda_lite/main.o";
   std::string set_o = std::string(tvm_p) + "/tutorials/cuda_lite/bsg_set_tile_x_y.o";
   std::string printf_o = std::string(tvm_p) + "/tutorials/cuda_lite/bsg_printf.o";
+  std::string tut_path = std::string(tvm_p) + "/tutorials/cuda_lite";
 
   std::string compiler_t = "-t -T " + manycore_path +"/software/spmd/common/link_dmem2.ld";
   std::string compiler_w = "-Wl,--defsym,bsg_group_size=4 -Wl,--defsym,_bsg_elf_dram_size=1207959552 -Wl,--defsym,_bsg_elf_vcache_size=294912 -Wl,--defsym,_bsg_elf_stack_ptr=0x00001ffc -Wl,--no-check-sections";
   //std::string compiler_misc = "-march=rv32imaf -nostdlib -nostartfiles -ffast-math";
   std::string compiler_misc = "-march=rv32imaf -nostartfiles -ffast-math";
-  std::string compiler_l = "-lc -lgcc -lm -l:crt.o -L " + manycore_path + "/software/spmd/common";
+  //std::string compiler_l = "-lc -lgcc -lm -l:crt.o -L " + manycore_path + "/software/spmd/common";
+  std::string compiler_l = "-lc -lgcc -lm -l:crt.o -L" + tut_path;
   std::string out_name = file_name_prefix + ".hbmc";
 
   cmd = compiler + " " + compiler_t + " " + compiler_w + " " + main_o + " " + set_o + " " + printf_o + " " + file_name_prefix + ".o ";
