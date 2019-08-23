34d0acd2dc28f3a4d1f93e0ae00adf85709ef2e9
Chien-Yu Lin <cyulin@cs.washington.edu>
2019-08-23 18:15:12 +0000

    update convnet

--- tutorials/cuda_lite/relay_convnet_cuda_lite.py
+++ tutorials/cuda_lite/relay_convnet_cuda_lite.py
@@ -8,16 +8,17 @@ from tvm.contrib.download import download_testdata
 from PIL import Image
 #from hb import ir_pass
 import time
+import sys
 
-dtype="float"
-network_scale = 0
+dtype="float32"
+network_scale = 2
 batch_size = 1
 num_class = 2
-image_shape = (3, 8, 8)
+image_shape = (3, 4, 4)
 data_shape = (batch_size,) + image_shape
 #data_shape = (batch_size, num_class)
-out_shape = (batch_size, num_class)
-#out_shape = (batch_size, ) + (1, 4, 4)
+#out_shape = (batch_size, num_class)
+out_shape = (batch_size, ) + (3, 2, 2)
 
 def print_stmt(stmt):
     print(stmt)
@@ -26,14 +27,15 @@ def print_stmt(stmt):
 
 img_url = 'https://github.com/dmlc/mxnet.js/blob/master/data/cat.png?raw=true'
 img_path = download_testdata(img_url, 'cat.png', module='data')
-img = Image.open(img_path).resize((8, 8))
+img = Image.open(img_path).resize((4, 4))
 x = np.array(img)
 x = np.moveaxis(x, 2, 0)
-x = x/255
 x = np.array(x)[np.newaxis, :, :, :]
+x = x/255
 #img_ycbcr = img.convert("YCbCr")  # convert to YCbCr
 #img_y, img_cb, img_cr = img_ycbcr.split()
 #x = np.array(img_y)[np.newaxis, np.newaxis, :, :]
+#x = x/255
 
 net, params = relay.testing.sdh_convnet.get_workload(
               batch_size=batch_size,
@@ -50,7 +52,6 @@ print(net.astext(show_meta_data=False))
 
 opt_level = 3
 target = tvm.target.cuda_lite()
-cuda_lite_start = time.time()
 with relay.build_config(opt_level=opt_level):
     #with tvm.build_config(add_lower_pass=[(1, ir_pass.inject_thread_loop)]):
     with tvm.build_config(add_lower_pass=[(1, print_stmt)]):
@@ -65,15 +66,14 @@ with relay.build_config(opt_level=opt_level):
         module.set_input("data", x.astype("float32"))
         module.set_input(**_params)
         # run
+        cuda_lite_start = time.time()
         module.run()
+        cuda_lite_end = time.time()
         # get output
         out_cuda_lite = module.get_output(0, tvm.nd.empty(out_shape)).asnumpy()
 
-cuda_lite_end = time.time()
-#exit()
 
 target = "llvm"
-cpu_start = time.time()
 with relay.build_config(opt_level=opt_level):
     graph, lib, _params = relay.build_module.build(net, target, params=params)
 
@@ -84,12 +84,14 @@ with relay.build_config(opt_level=opt_level):
     module.set_input("data", x.astype("float32"))
     module.set_input(**_params)
     # run
+    cpu_start = time.time()
     module.run()
+    cpu_end = time.time()
     # get output
     out_cpu = module.get_output(0, tvm.nd.empty(out_shape)).asnumpy()
 
-cpu_end = time.time()
 
+np.set_printoptions(threshold=sys.maxsize)
 print("CUDA-Lite Outputs:")
 print(out_cuda_lite.flatten())
 print("CUDA-Lite Build + Run time: %f" % (cuda_lite_end - cuda_lite_start))
@@ -98,7 +100,11 @@ print("CPU Outputs:")
 print(out_cpu.flatten())
 print("CPU Build + Run time: %f" % (cpu_end - cpu_start))
 
-if not tvm.testing.assert_allclose(out_cuda_lite, out_cpu, rtol=1e-3, atol=1e-2):
+print(np.max(out_cuda_lite.flatten()), np.min(out_cuda_lite.flatten()))
+print(np.max(out_cpu.flatten()), np.min(out_cpu.flatten()))
+#print("Setdiff1d:")
+#print(np.setdiff1d(out_cuda_lite.flatten(), out_cpu.flatten()))
+if not tvm.testing.assert_allclose(out_cuda_lite.flatten(), out_cpu.flatten(), rtol=1e-3, atol=1e-2):
     print("CUDA-Lite RESULTS MATCH CPU RESULTS (WITHIN TOLERANCE)")
 
 exit()
