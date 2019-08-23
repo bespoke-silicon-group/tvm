bb940d0d48b32bd47e2ef6a8fe25256ed745a40f
Chien-Yu Lin <cyulin@cs.washington.edu>
2019-08-23 18:15:32 +0000

    test vec add with float

--- tutorials/cuda_lite/vec_add_cuda_lite.py
+++ tutorials/cuda_lite/vec_add_cuda_lite.py
@@ -12,6 +12,7 @@ from __future__ import absolute_import, print_function
 
 import tvm
 import numpy as np
+import sys
 
 # Global declarations of environment.
 
@@ -19,7 +20,7 @@ tgt_host="llvm"
 # Use cuda_litr or hbmc to generate code for hammerblade
 tgt="cuda_lite"
 
-dtype = 'int32'
+dtype = 'float32'
 
 n = tvm.var("n")
 A = tvm.placeholder((n,), dtype=dtype, name='A')
@@ -27,7 +28,7 @@ B = tvm.placeholder((n,), dtype=dtype, name='B')
 C = tvm.compute(A.shape, lambda i: A[i] + B[i], name="C")
 
 s = tvm.create_schedule(C.op)
-bx, tx = s[C].split(C.op.axis[0], factor=4)
+bx, tx = s[C].split(C.op.axis[0], factor=2)
 
 if tgt == "cuda_lite" or tgt == "hbmc":
   s[C].bind(bx, tvm.thread_axis("blockIdx.x"))
@@ -42,13 +43,17 @@ if tgt == "cuda_lite" or tgt.startswith('opencl'):
 
 ctx = tvm.context(tgt, 0)
 
+np.set_printoptions(threshold=sys.maxsize)
 n = 16
-a = tvm.nd.array(np.random.randint(10, size=n).astype(A.dtype), ctx)
+#a = tvm.nd.array(np.random.randint(10, size=n).astype(A.dtype), ctx)
+a = tvm.nd.array(np.random.uniform(-1, 1, size=n).astype("float32"), ctx)
 print(a)
-b = tvm.nd.array(np.random.randint(10, size=n).astype(B.dtype), ctx)
+#b = tvm.nd.array(np.random.randint(10, size=n).astype(B.dtype), ctx)
+b = tvm.nd.array(np.random.uniform(-1, 1, size=n).astype("float32"), ctx)
 print(b)
 c = tvm.nd.array(np.zeros(n, dtype=C.dtype), ctx)
 fadd(a, b, c)
 print(c)
-tvm.testing.assert_allclose(c.asnumpy(), a.asnumpy() + b.asnumpy())
+print(a.asnumpy() + b.asnumpy())
+tvm.testing.assert_allclose(c.asnumpy(), a.asnumpy() + b.asnumpy(), rtol=1e-3, atol=1e-2)
 
